81f101732f35e2850d7d819d226d651e4fd1e6be
Achim <git@achimonline.de>
2021-07-11 20:17:49 +0200

    add "ignores" for "find_unused_messages"

--- mozdev/files/find_unused_messages.sh
+++ mozdev/files/find_unused_messages.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+SOURCE_PATH=${1}
+IGNORE_LINES=(${2//;/ })
+
 TMP_FILE=$(mktemp)
 
-find "${1}" -iname "messages.json" -exec sh -c "cat '{}' | jq 'keys' >> ${TMP_FILE}" \;
+find "${SOURCE_PATH}" -iname "messages.json" -exec sh -c "cat '{}' | jq 'keys' >> ${TMP_FILE}" \;
 
 LINES=$(cat ${TMP_FILE} || sort || uniq)
 
@@ -11,6 +14,10 @@ for LINE in ${LINES}; do
   if [[ ${LINE} == *\"*\"* ]]; then
     LINE=${LINE#*\"}
     LINE=${LINE%\"*}
+
+    if [[ " ${IGNORE_LINES[@]} " =~ " ${LINE} " ]]; then
+        continue
+    fi
 
     grep -r "${LINE}" "${1}" | grep -vq "messages.json"
 
